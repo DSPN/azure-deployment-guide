149baab5847e214eb4e7fc8c88190e6f8c693067
Matt Kennedy <matt.kennedy@datastax.com>
2014-06-27 00:02:15 -0400

    Added JNA to Java set up.

--- scripts/setup_java.sh
+++ scripts/setup_java.sh
@@ -9,12 +9,17 @@ APPDOMAIN=datastax-test.cloudapp.net
 USERID=datastax
 KEYFILE=~/.azurekeys/datastax-test.key
 JAVA_RPM=~/Downloads/jre-7u60-linux-x64.rpm
+JNA=~/Downloads/jna-4.1.0.jar
 
 for nodeport in {10001..10050}; 
 do 
 	scp -P ${nodeport} -i ${KEYFILE} ${JAVA_RPM} ${USERID}@${APPDOMAIN}:
+	scp -P ${nodeport} -i ${KEYFILE} ${JNA} ${USERID}@${APPDOMAIN}:
 	ssh -t -i ${KEYFILE} ${USERID}@${APPDOMAIN} -p ${nodeport} 'sudo yum -y remove java*'
 	ssh -t -i ${KEYFILE} ${USERID}@${APPDOMAIN} -p ${nodeport} "sudo yum -y install $(basename $JAVA_RPM)"
+	ssh -t -i ${KEYFILE} ${USERID}@${APPDOMAIN} -p ${nodeport} "sudo mkdir -p /usr/share/dse/cassandra/lib"
+	ssh -t -i ${KEYFILE} ${USERID}@${APPDOMAIN} -p ${nodeport} "sudo cp $(basename $JNA) /usr/share/dse/cassandra/lib/jna.jar"
 	ssh -i ${KEYFILE} ${USERID}@${APPDOMAIN} -p ${nodeport} 'java -version'
+
 
 done
